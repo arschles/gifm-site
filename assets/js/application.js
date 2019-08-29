806c941d387bb8945739179fe99be31f68313ec5
Aaron Schlesinger <aaron@ecomaz.net>
2019-08-29 15:14:15 -0700

    forgot to actually start turbolinks

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,6 +1,7 @@
 require("expose-loader?$!expose-loader?jQuery!jquery");
 require("bootstrap/dist/js/bootstrap.bundle.js");
+var Turbolinks = require("turbolinks")
 
 $(() => {
-
+    Turbolinks.start()
 });
