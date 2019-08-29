c38646701fccf9e11517986eaf3a7a10ff03710b
Aaron Schlesinger <aaron@ecomaz.net>
2019-08-29 15:36:31 -0700

    adding a TODO

--- assets/js/application.js
+++ assets/js/application.js
@@ -3,5 +3,7 @@ require("bootstrap/dist/js/bootstrap.bundle.js");
 var Turbolinks = require("turbolinks")
 
 $(() => {
+    // TODO: there are gotchas with this, check out the following:
+    // https://www.honeybadger.io/blog/turbolinks/
     Turbolinks.start()
 });
