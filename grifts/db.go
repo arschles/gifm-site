0c7e9c100b51db82a717c90e4015a535af97aa45
Aaron Schlesinger <aaron@ecomaz.net>
2019-09-02 16:17:31 -0700

    adding a grift for seeding the DB

--- grifts/db.go
+++ grifts/db.go
@@ -1,6 +1,9 @@
 package grifts
 
 import (
+	"fmt"
+
+	"github.com/arschles/go-in-5-minutes-site/models"
 	"github.com/markbates/grift/grift"
 )
 
@@ -8,7 +11,32 @@ var _ = grift.Namespace("db", func() {
 
 	grift.Desc("seed", "Seeds a database")
 	grift.Add("seed", func(c *grift.Context) error {
-		// Add DB seeding stuff here
+		screencasts := []models.Screencast{
+			{
+				EpisodeNum: 1,
+				Title:      "This is episode 1!",
+				Intro:      "Here's an intro",
+				Markdown:   "# Here's some markdown",
+			},
+			{
+				EpisodeNum: 1,
+				Title:      "This is episode 2!",
+				Intro:      "Here's an intro",
+				Markdown:   "# Here's some markdown",
+			},
+			{
+				EpisodeNum: 1,
+				Title:      "This is episode 3!",
+				Intro:      "Here's an intro",
+				Markdown:   "# Here's some markdown",
+			},
+		}
+		for _, screencast := range screencasts {
+			if _, err := models.DB.ValidateAndCreate(&screencast); err != nil {
+				return err
+			}
+		}
+		fmt.Println("Seeded the DB with", len(screencasts), "screencast(s)")
 		return nil
 	})
 
