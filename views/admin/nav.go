f9597748ab829bfbc19b89e34957097b71ae2bef
Aaron Schlesinger <aaron@ecomaz.net>
2019-10-13 18:56:53 -0700

    Make the nav header simpler

--- views/admin/nav.go
+++ views/admin/nav.go
@@ -3,26 +3,16 @@ package admin
 import (
 	"github.com/arschles/gifm-site/pkg/render"
 	"github.com/arschles/gifm-site/pkg/tags"
-	"github.com/arschles/gifm-site/views/components/bootstrap"
 )
 
 func nav() render.Elt {
-	stdCol := bootstrap.NewCol(render.TagOpts{"class": "col-sm"})
-	return bootstrap.NewGrid(render.TagOpts{
-		"class": "mt-4",
-	}).WithRow(
-		bootstrap.NewRow(render.EmptyOpts()).WithCol(
-			stdCol.WithChild(
-				tags.A("/admin", render.EmptyOpts(), "Admin Home"),
-			),
-		).WithCol(
-			stdCol.WithChild(
-				tags.A(
-					"/admin/screencasts/new",
-					render.EmptyOpts(),
-					"Create a new screencast",
-				),
-			),
+	links := []render.Elt{
+		tags.A("/admin", render.EmptyOpts(), "Admin Home"),
+		tags.A(
+			"/admin/screencasts/new",
+			render.TagOpts{"class": "ml-4"},
+			"Create a new screencast",
 		),
-	)
+	}
+	return tags.Div(render.EmptyOpts(), links...)
 }
